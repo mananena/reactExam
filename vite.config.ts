7a1280859c6051d6520d0304f63d85c6cff4347e
mananena <113594753+mananena@users.noreply.github.com>
2024-05-14 22:36:01 +0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["logo.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+          name: "Web",
+          short_name: "Web",
+          description: "Website ",
+          theme_color: "#ffffff",
+          start_url: "/",
+          icons: [
+            {
+              src: 'logo192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'logo512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            },
+            {
+              src: 'logo512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable'
+            }
+          ],
+      },
+    }),
+  ],
 })
